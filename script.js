cf06bbe951d4fd1b7bcd347e49c7e885c0e8eabe
DVs07 <aldopablo7@hotmail.com>
2020-11-20 02:33:05 -0300

    Added section home, nav menu animation & styles css

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function linkAction(){
     navMenu.classList.remove('show');
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*      Scroll sections active link     */
+const sections = document.querySelectorAll('section[id]');
+window.addEventListener('scroll', scrollActive);
+
+function scrollActive(){
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionID= current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('nav-menu a[href*='+ sectionID +']').claslist.add('active');
+        }else{
+            document.querySelector('nav-menu a[href*='+ sectionID +']').claslist.remove('active');
+        }
+    })
+}
